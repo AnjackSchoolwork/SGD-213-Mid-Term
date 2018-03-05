c938d209706a3326c3b8c28869d80dc81c47cd9b
Anjack <gary.reif@gmail.com>
2018-03-05 14:16:33 -0500

    Init

---
+++ Mid-Term_Project/Assets/Scripts/PickupController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour {
+
+    public float rotate_speed;
+    public float bob_height;
+    
+
+    private ArrayList pickups;
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject pickup_instance in GameObject.FindGameObjectsWithTag("pickup"))
+        {
+            pickups.Add(pickup_instance);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(GameObject pickup_instance in GameObject.FindGameObjectsWithTag("pickup"))
+        {
+            pickup_instance.transform.Rotate(Vector3.up, rotate_speed * Time.deltaTime);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "player")
+        {
+            other.gameObject.GetComponent<PlayerStatsController>().score += 10;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Mid-Term_Project/Assets/Scripts/PlayerStatsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatsController : MonoBehaviour {
+    
+
+    public int score;
+
+    public int health;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        health = 100;
+	}
+
+    
+}
